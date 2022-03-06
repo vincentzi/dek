66585fb17bf2c4438928b0f5611df7463df16a18
Vincent Zi <workwithvincentzi@gmail.com>
2022-03-06 10:00:54 -0500

    Add test for Timer util

---
+++ tests/utils/test_timer.py
@@ -0,0 +1,14 @@
+from time import sleep
+
+from freezegun import freeze_time
+from dek.utils.timer import Timer
+
+@freeze_time('2022-01-01 00:00:10', auto_tick_seconds=3)
+def test_timer_context_manager(capsys):
+    with Timer(text='time: {:0.2f} seconds', logger=print):
+        sleep(3)
+
+    captured = capsys.readouterr()
+    assert captured.out == "time: 3.00 seconds\n"
+
+
